5e03556be12ef4411483261a716600eacebdf228
Pavel07-chyden <pates1993@mail.ru>
2022-02-27 16:28:26 +0300

    new add data server

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults({
-   static: './build'
-});
+const express = require('express');
+const path = require('path');
+
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(jsonServer.rewriter({
-   '/api/*': '/$1',
-}))
-server.use(router);
-server.listen(port, () => {
-   console.log(`Server is running {port}`);
-});
+const app = express();
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
